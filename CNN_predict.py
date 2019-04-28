ed488f793ffaf269f6ff7cfc1307c20782847c75
jek55rus jek55rus <markin95evgeny@gmail.com>
2019-04-28 19:49:44 +0300

    added precision, recall, F-measure and MCC

--- CNN_predict.py
+++ CNN_predict.py
@@ -3,26 +3,45 @@ import os
 from keras.initializers import glorot_uniform
 from keras.utils import CustomObjectScope
 from keras.preprocessing import image
+from keras.utils.np_utils import to_categorical
 import numpy as np
 import time
+import pandas as pd
+import pickle
+
 with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
     model = load_model('model_1.h5')
 print("Model loading is over")
 input_size = (96, 96)
-test_folderpath = 'D:/Hearts of Iron III/dowmloads/CERTH_ImageBlurDataset/AdditionalDataset/'
 start = time.time()
-# check method efficiency on the same stuff as the classic CV method
-for filename in os.listdir(test_folderpath):
-    impath = test_folderpath + filename
-    img = image.load_img(impath, color_mode="grayscale", target_size=input_size)
-    img = (1 / 255) * np.asarray(img)
-    img = img.reshape(1, 96, 96, 1)
-    pred = model.predict(img, verbose=1)
-    # debugging printing
-    print(pred)
-    if pred[0][0] > pred[0][1]:
-        print(filename + " was defined as a sharp one")
-    else:
-        print(filename + " was defined as a blurry one")
+with open('X_test.pkl', 'rb') as picklefile:
+    X_test = pickle.load(picklefile)
+with open('y_test.pkl', 'rb') as picklefile:
+    y_test = pickle.load(picklefile)
+testdata = np.stack(X_test)
+testdata = testdata.reshape(1480, 96, 96, 1)
+preds = model.predict(testdata, verbose=1)
+# identifying other metrics
+tn = 0  # true negative
+tp = 0  # true positive
+fn = 0  # false negative
+fp = 0  # false positive
+for i in range(0, 1480):
+    if (preds[i][0] > preds[i][1] and y_test[i] == 0):
+        tn += 1
+    elif (preds[i][0] > preds[i][1] and y_test[i] == 1):
+        fn += 1
+    elif (preds[i][0] < preds[i][1] and y_test[i] == 0):
+        fp += 1
+    elif (preds[i][0] < preds[i][1] and y_test[i] == 1):
+        tp += 1
 end = time.time()
-print("Elapsed time: ", end-start)
+print("Elapsed time: ", end - start)
+Precision = tp/(tp+fp)
+Recall = tp/(tp+fn)
+F = 2*Precision*Recall/(Precision + Recall)
+MCC = (tp*tn-fp*fn)/np.sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))
+print("Precision = ", Precision)
+print("Recall = ", Recall)
+print("F-measure = ", F)
+print("MCC = ", MCC)
